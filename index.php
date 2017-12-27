48ab9de8f068f1e00f170df714e663d86851726b
Jamie Law <jamiepeterlaw@gmail.com>
2017-12-27 20:19:37 +0000

    innit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Meet - Chat - Discover</title>
+    <link rel="stylesheet" href="/Style/master.css">
+  </head>
+  <body>
+    <ul>
+      <li><a href="register.php">Register</a></li>
+      <li><a href="login.php">Login</a></li>
+      <li><a href="profile.php">Prile</a></li>
+      <li><a href="meet.php">Meet People!</a></li>
+    </ul>
+
+
+
+    <h1>Online Users</h1>
+    <br><hr>
+    <p>foo</p>
+
+  <hr>
+  <br>
+  </body>
+</html>
